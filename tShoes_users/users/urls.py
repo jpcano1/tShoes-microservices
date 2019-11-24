c8fffdeb51a129a054d15a6b3356814e5c742f44
Juan Pablo Cano <jp.cano@uniandes.edu.co>
2019-11-24 12:17:14 -0500

    routes

--- tShoes_users/users/urls.py
+++ tShoes_users/users/urls.py
@@ -8,9 +8,13 @@ from rest_framework.routers import DefaultRouter
 
 # Views
 from .views import users as user_views
+from .views import designers as designer_views
+from .views import customers as customer_views
 
 router = DefaultRouter()
 router.register(r'users', user_views.UserViewSet)
+router.register(r'designers', designer_views.DesignerViewSet)
+router.register(r'customers', customer_views.CustomerViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
